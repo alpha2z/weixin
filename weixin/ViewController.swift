8b7c06c763e681b57c0c643236fb0fcb0ad6dd0a
riddickzhou <240581469@qq.com>
2014-10-19 10:23:34 +0800

    Init project.

---
+++ weixin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weixin
+//
+//  Created by riddick on 14-10-19.
+//  Copyright (c) 2014年 riddick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
